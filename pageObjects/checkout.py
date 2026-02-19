24b0308328211cae3a668278d2cb64add00c374d
swakshan <swakshan01022003@gmail.com>
2026-02-19 14:10:43 +0530

    changed

--- pageObjects/checkout.py
+++ pageObjects/checkout.py
@@ -26,5 +26,5 @@ class CheckoutPage(Util):
         self.driver.find_element(*self.checkBox).click()
         self.driver.find_element(*self.submitBtn).click()
         message = self.driver.find_element(*self.msg).text 
-        print(message)
+        print("The message: ",message)
         assert "Success" in message
